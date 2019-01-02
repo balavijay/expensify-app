5b2d00d07c61122ca9d890f1c8f71d2df5563120
Balavijay S <balavijay@gmail.com>
2019-01-02 20:21:06 +0530

    Added summary feature

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -6,7 +6,7 @@ import numeral from 'numeral';
 const ExpenseListItem = ({ id, desc, amount, createdAt} , props) =>  (
        <div>
          <Link to={`/edit/${id}`}><h3> {desc}</h3></Link> 
-         <p>{numeral(amount).format('$0,0.00')} - {moment(createdAt).format('Do-MMMM-YYYY')}  </p>
+         <p>{numeral(amount).format('$0,0.00')} - {moment(createdAt).format('Do-MMM-YYYY')}  </p>
          
        </div>
    ); 
